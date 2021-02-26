08786e460a924ef6152a1a7538b669cf967f5989
“draganaisak” <dragana.isak@gmail.com>
2021-02-26 21:57:35 +0100

    changed interceptors

--- src/services/BaseService.js
+++ src/services/BaseService.js
@@ -14,14 +14,24 @@ class BaseService {
             return request;
         });
 
-        this.http.interceptors.response.use(async function(response) {
-            const token = JSON.parse(localStorage.getItem('token'));
-            if (token && response.status === 401) {
-                const { data } = await this.http.post('/refresh-token');
-                localStorage.setItem('token', data.token);
-            }
-            return response;
-        });
+        // this.http.interceptors.response.use(async (response) => {
+        //     console.log('response interceptor')
+        //     const token = JSON.parse(localStorage.getItem('token'));
+        //     console.log('token', token)
+        //     if (token && response.status === 401) {
+        //         console.log('sending request')
+        //         try {
+        //             console.log('sending request')
+        //             const response = await this.http.post('/refresh-token');
+        //             console.log('got response')
+        //             localStorage.setItem('token', response.data.token);
+    
+        //         } catch (err) {
+        //             console.log('err', { err })
+        //         }
+        //     }
+        //     return response;
+        // });
     }
 }
 
