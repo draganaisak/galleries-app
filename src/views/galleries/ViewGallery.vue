0551e2ad6bda1a45682b4c56904d4bbb9db8436e
draganaisak <dragana.isak@gmail.com>
2021-02-26 12:52:14 +0100

    added view gallery page with gallery details and image slider

--- src/views/galleries/ViewGallery.vue
+++ src/views/galleries/ViewGallery.vue
@@ -1,16 +1,89 @@
 <template>
-    <div>
-        <h1>Gallery Photoes</h1>
+    <div class="container">
+        <h1>{{ gallery.name }}</h1>
+        <router-link v-if="gallery.user" :to="`/author/${gallery.user_id}`">
+            {{ gallery.user.first_name }} {{ gallery.user.last_name }}
+        </router-link>
+        <p>{{ gallery.created_at | formatDate }}</p>
+        <p>{{ gallery.description }}</p>
+        <div id="images" class="carousel slide" data-ride="carousel">
+            <div class="carousel-inner">
+                <div class="carousel-item" v-for="(image, index) in gallery.images" :key="index" @click="setCurrentItem(index)" :class="{ 'active': index === currentItem }">
+                    <a :href="image.url" target="_blank">
+                        <img :src="image.url" class="d-block w-50" alt="photo">
+                    </a>
+                </div>
+            </div>
+            <a class="carousel-control-prev" role="button" data-slide="prev" @click="changeCurrentItem()">
+                <span class="carousel-control-prev-icon controls" aria-hidden="true"></span>
+            </a>
+            <a class="carousel-control-next" role="button" data-slide="next" @click="changeCurrentItem()">
+                <span class="carousel-control-next-icon controls" aria-hidden="true"></span>
+            </a>
+        </div>
+        <div v-if="activeUser && gallery.user_id === activeUser.id">
+            <button type="button" @click="deleteThisGallery(gallery.id)">Delete</button>
+            <router-link :to="`/edit-gallery/${gallery.id}`">Edit</router-link>
+        </div>
     </div>
 </template>
 
 <script>
-export default {
-    name: 'show-gallery',
-}
+    import {mapActions, mapGetters} from "vuex";
+    export default {
+        name: 'view-gallery',
+        data() {
+            return {
+                currentItem: 0,
+                id: +this.$route.params.id
+            }
+        },
+        computed: {
+            ...mapGetters('galleries', ['gallery']),
+            ...mapGetters('auth', ['activeUser'])
+        },
+        methods: {
+            setCurrentItem(index) {
+                console.log(index);
+                this.currentItem = index;
+            },
+
+            changeCurrentItem() {
+                if(this.currentItem < this.gallery.images.length && this.currentItem <= 0) {
+                    this.currentItem += 1;
+                } else if(this.currentItem < this.gallery.images.length) {
+                    this.currentItem = 0;
+                }
+            },
+            ...mapActions('galleries', ['getGalleryById']),
+            ...mapActions('galleries', ['deleteGallery']),
+            ...mapActions('auth', ['getActiveUser']),
+
+            deleteThisGallery(id) {
+                console.log(id);
+                try {
+                    this.deleteGallery(id);
+                    this.$router.push('/my-galleries');
+                } catch(e) {
+                    console.log('Not deleted');
+                }
+            }
+        },
+        created() {
+            this.getGalleryById(this.id);
+            this.getActiveUser()
+        },
+    }
 
 </script>
 
 <style>
+    .carousel-item a > img {
+        margin: 0 auto;
+    }
 
+    .controls {
+        background-color: darkcyan;
+        font-size: 20px;
+    }
 </style>
