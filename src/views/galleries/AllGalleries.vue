81b5bef9005aa9e2d50582f4258ecae9a8303dee
draganaisak <dragana.isak@gmail.com>
2021-02-26 13:29:20 +0100

    added logic for load more button

--- src/views/galleries/AllGalleries.vue
+++ src/views/galleries/AllGalleries.vue
@@ -1,10 +1,17 @@
+
 <template>
     <div class="container">
-        <div class="card mb-5" v-for="gallery in galleries" :key="gallery.id">
-            <router-link :to="`/galleries/${gallery.id}`">{{ gallery.name }}</router-link>
-            <img :src="gallery.images[0].url" alt="photo">
-            <router-link :to="`/authors/${gallery.user_id}`">{{ gallery.user.first_name }} {{ gallery.user.last_name }}</router-link>
-            <p>{{ gallery.created_at | formatDate }}</p>
+        <div v-if="galleriesData">
+            <div class="card mb-5" v-for="gallery in galleriesData" :key="gallery.id">
+                <router-link :to="`/galleries/${gallery.id}`">{{ gallery.name }}</router-link>
+                <img :src="gallery.images[0].url" alt="photo">
+                <router-link :to="`/authors/${gallery.user_id}`">{{ gallery.user.first_name }} {{ gallery.user.last_name }}</router-link>
+                <p>{{ gallery.created_at | formatDate }}</p>
+            </div>
+            <button v-if="galleries.next_page_url" type="button" @click="loadMore">Load more</button>
+        </div>
+        <div v-else>
+            <p>There are no galleries.</p>
         </div>
     </div>
 </template>
@@ -16,11 +23,17 @@
     export default {
         name: "all-galleries",
         computed: {
-            ...mapGetters('galleries', ['galleries'])
+            ...mapGetters('galleries', ['galleries']),
+            ...mapGetters('galleries', ['galleriesData'])
         },
         async created() {
             await store.dispatch('galleries/getGalleries');
             console.log(this.galleries);
+        },
+        methods: {
+            async loadMore() {
+                await store.dispatch('galleries/addMoreGalleries');
+            }
         }
     }
 </script>
